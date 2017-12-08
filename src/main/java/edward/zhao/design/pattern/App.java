9c2a0fb8a7266d57b00c93d16832faa6dc9ae485
zhaokl <zhaokunlun@isesol.com>
2017-12-08 14:00:11 +0800

    init

---
+++ src/main/java/edward/zhao/design/pattern/App.java
@@ -0,0 +1,13 @@
+package edward.zhao.design.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
