5b7e69b398928556ff9d2d711e78448cd60b5962
zhaokl <zhaokunlun@isesol.com>
2017-12-29 16:38:46 +0800

    1229 javap

---
+++ src/main/java/tool/TestClass.java
@@ -0,0 +1,17 @@
+package tool;
+
+/**
+ * @author zhaokl
+ * @version V1.0
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: TODO
+ * @date ${date} ${time}
+ */
+public class TestClass {
+    int testInt;
+
+    public void test() {
+        testInt = 1;
+    }
+}
---
+++ src/main/java/tool/TestJavaP1227.java
@@ -0,0 +1,13 @@
+package tool;
+
+/**
+ * @author zhaokl
+ * @version V1.0
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: TODO
+ * @date ${date} ${time}
+ */
+public class TestJavaP1227 {
+
+}
